47b1c11084f75b739006d7b964632c067ef5f4e8
andiausrust <familie.mayer@aon.at>
2018-10-27 15:14:22 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main(){
+	links := []string{
+		"http://amazon.com",
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	/*for {
+		go checkLink(<-c, c)
+	}*/
+
+	// the same as above but better readable
+	for l:= range c {
+		go func(link string) {							// function literal (anonymous function)
+			time.Sleep(5 * time.Second)  			// pause the code for 5 seconds
+			checkLink(l, c)
+		}(l)										// pass l
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+}
